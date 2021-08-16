cadfd13570dec4b34eca2abec014a2afd6763f93
jp3parker <jp3parker@gmail.com>
2021-08-16 18:34:00 -0500

    changed checkWin()

--- game.cpp
+++ game.cpp
@@ -22,39 +22,27 @@ int available(char board[][WIDTH], int column) {
     return available;
 }
 
+//this code is long and inefficient -- should probably change it
 bool checkWin(char board[][WIDTH]) {
     bool won = false;
     for (int i=0; i<WIDTH; ++i) {
         for (int j=HEIGHT-1; j>=0 and board[j][i] != ' ' and !won; --j) {
             char color = board[j][i];
-            std::string how;
-            if ((i-3) >= 0 and (j-3) >= 0) {
-                if (board[j-1][i-1] == color and board[j-2][i-2] == color and board[j-3][i-3] == color) {
-                    won = true;
-                    how = "left diag";
-                }
-            }
             if ((j-3) >= 0) {
                 if (board[j-1][i] == color and board[j-2][i] == color and board[j-3][i] == color) {
                     won = true;
-                    how = "up";
                 }
-            }
-            if ((i+3) < WIDTH and (j-3) >= 0) {
-                if (board[j-1][i+1] == color and board[j-2][i+2] == color and board[j-3][i+3] == color) {
+                if ((i-3) >= 0 and board[j-1][i-1] == color and board[j-2][i-2] == color and board[j-3][i-3] == color) {
                     won = true;
-                    how = "right diag";
                 }
             }
             if ((i+3) < WIDTH) {
                 if (board[j][i+1] == color and board[j][i+2] == color and board[j][i+3] == color) {
                     won = true;
-                    how = "right";
                 }
-            }
-            if(won) {
-                std::cout << "(" << i << "," << j << ")" << std::endl;
-                std::cout << how << std::endl;
+                if ((j-3) >= 0 and board[j-1][i+1] == color and board[j-2][i+2] == color and board[j-3][i+3] == color) {
+                    won = true;
+                }
             }
         }
     }
